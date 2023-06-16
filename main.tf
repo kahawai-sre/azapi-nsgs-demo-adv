bf59afeb99036218adcd8ba09e9a2fc94546d8ba
kahawai-sre <jturver@hotmail.com>
2023-06-17 09:48:15 +1200

    initial-1.1

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "resourcegroup-azapi" {
   tags      = each.value.tags
 }
 
-#// 2. Deploy ASGs as per "asgs_config_map"
+#// 2. Deploy ASGs as per "asgs_config_map" defined in locals.tf
 module "asg-azapi" {
   for_each = local.asgs_config_map
   source   = "./modules/asg-azapi/"
@@ -59,16 +59,16 @@ module "nsg-azapi" {
   depends_on = [module.resourcegroup-azapi, module.asg-azapi, data.azurerm_subscriptions.available]
 }
 
-#// 4. Deploy NSG=>Subnet associations as per nsg_subnet_associations_flatten defined in locals.tf
+#// 4. Deploy NSG=>Subnet associations as per "nsg_subnet_associations_map" defined in locals.tf
 #// NOTE: using azapi_update_resource here. This has limitation as described here: https://github.com/Azure/terraform-provider-azapi/issues/204
 module "nsg-subnetassociation-azapi" {
   for_each = local.nsg_subnet_associations_map
   source   = "./modules/nsg-subnetassociation-azapi/"
   #// NOTE: line below is a WIP. Need to determine how to remove the properties.networkSecurityGroup property from the target subnet configuration using azapi_update_resource, or otherwise
-  #// asper limitation https://github.com/Azure/terraform-provider-azapi/issues/204
-  nsg_id     = each.value.associated_state == true ? module.nsg-azapi-new[each.value.nsg_name].nsg_id : "" #// <<<=== check associated_state flag. If associated_state = true, do the assignment, else, clear the assignment (for Day2 ops where want to clear subnet => NSG link)
+  #// as per limitation described here https://github.com/Azure/terraform-provider-azapi/issues/204
+  nsg_id     = each.value.associated_state == true ? module.nsg-azapi[each.value.nsg_name].nsg_id : "" #// <<<=== check value of the "associated_state" flag. If associated_state = true, do the assignment, else, clear the assignment e.g. remove networkSecurityGroup property  (for Day2 ops where want to clear subnet => NSG association)
   subnet_id  = each.value.subnet_id
-  depends_on = [module.nsg-azapi-new]
+  depends_on = [module.nsg-azapi]
 }
 
 #// TO DO:
