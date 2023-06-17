d17d5a36351e3cee6e911a61bd50d26c4d7c5e63
kahawai-sre <jturver@hotmail.com>
2023-06-17 12:59:12 +1200

    initial1.4

--- vars.tf
+++ vars.tf
@@ -1,16 +1,16 @@
 
 
-#// Path to yaml file defining Resource Groups to deploy and manage
+#// Path to yaml file defining Resource Groups, to deploy and manage
 variable "resourcegroups_yaml_file_path" {
   default = "./config/resourcegroups.yaml"
 }
 
-#// Path to yaml file defining ASGs and associated securityRules to deploy and manage
+#// Path to yaml file defining ASGs and associated securityRules, to deploy and manage
 variable "asgs_yaml_file_path" {
   default = "./config/asgs.yaml"
 }
 
-#// Path to yaml file defining NSGs and associated securityRules to deploy and manage
+#// Path to yaml file defining NSGs, NSG securityRules, and NSG => subnet associations, to deploy and manage
 variable "nsgs_yaml_file_path" {
   default = "./config/nsgs.yaml"
 }
